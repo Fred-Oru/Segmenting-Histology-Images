383dcc523c9ec445c46a79759c945a2b2d868e81
Fred-Oru <fred@ai4better.com>
2020-03-31 14:50:10 +0200

    Detection upstream work

--- Code/glomerulus.py
+++ Code/glomerulus.py
@@ -18,8 +18,8 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
     # Resume training a model that you had trained earlier
     python3 glomerulus.py train --dataset=/path/to/dataset --subset=train --weights=last
 
-    # Generate submission file
-    python3 glomerulus.py detect --dataset=/path/to/dataset --subset=train --weights=<last or /path/to/weights.h5>
+    # Generate masks and .roi files
+    python3 glomerulus.py detect --dataset=/path/to/dataset --subset=test --weights=<last or /path/to/weights.h5>
 """
 
 # Set matplotlib backend
@@ -39,6 +39,10 @@ import datetime
 import numpy as np
 import skimage.io
 from imgaug import augmenters as iaa
+# import pickle
+from cv2 import findContours
+from shutil import copyfile
+import zipfile
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
@@ -56,6 +60,10 @@ COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
 DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Results directory
+# Save detection outputs files here
+RESULTS_DIR = os.path.join(ROOT_DIR, "results/")
 
 # The dataset doesn't have a standard train/val split, so I picked
 # a variety of images to surve as a validation set.
@@ -101,9 +109,9 @@ class GlomerulusConfig(Config):
 
     # Input image resizing
     # Random crops of size 512x512
-    IMAGE_RESIZE_MODE = "square"
-    IMAGE_MIN_DIM = 1000
-    IMAGE_MAX_DIM = 1920
+    IMAGE_RESIZE_MODE = "crop"
+    IMAGE_MIN_DIM = 1024
+    IMAGE_MAX_DIM = 1024
     IMAGE_MIN_SCALE = 0
 
     # Length of square anchor side in pixels
@@ -165,9 +173,17 @@ class GlomerulusDataset(utils.Dataset):
         """Load a subset of the glomeruli dataset.
         dataset_dir: Root directory of the dataset
         subset: Subset to load. Either one of train, val or test
-            * train : all images in train except ids starting with VAL_IMAGE_STARTS_WITH
-            * valid : images starting with VAL_IMAGE_STARTS_WITH
-            * test : todo
+            * train : all images in "train" subset except ids starting with VAL_IMAGE_STARTS_WITH
+            * valid : images in "train" subset starting with VAL_IMAGE_STARTS_WITH
+            * other : all images in "other" subset
+        The "train" subset is organized in the following way :
+            * one folder per image (named after the name of the image)
+            * one subfolder 'images' containing the image with a .jpg format
+            * one subfolder 'masks' containing them masks with a .png format
+            * one subfolder 'rois' containing the rois with a .roi format (not used)
+        The other folder is a plain folder containing images to run detection on
+        The result folder will be organized as the "train" folder, with one folder
+        per image in the other folder
         """
         # Add classes. We have one class.
         # Naming the dataset glomerulus, and the class glomerulus
@@ -177,21 +193,29 @@ class GlomerulusDataset(utils.Dataset):
         # "val": use hard-coded list VAL_IMAGE_STARTS_WITH
         # "train": use data from train minus the hard-coded list VAL_IMAGE_STARTS_WITH
         # else: use the data from the specified sub-directory
-        assert subset in ["train", "val", "test"]
         subset_dir = "train" if subset in ["train", "val"] else subset
         dataset_dir = os.path.join(dataset_dir, subset_dir)
-        image_ids = next(os.walk(dataset_dir))[1]
         if subset_dir == "train":
+            image_ids = next(os.walk(dataset_dir))[1] # on directory per image
             val_ids = [id for id in image_ids for start in VAL_IMAGE_STARTS_WITH if id.startswith(start) ]
             non_val_ids = [id for id in image_ids if id not in val_ids]
             images_ids = val_ids if subset == "val" else non_val_ids
+        else:
+            image_ids = next(os.walk(dataset_dir))[2] # files
+            image_ids = [id[:-4] for id in image_ids if id.endswith('.jpg')]
 
         # Add images
         for image_id in image_ids:
+
+            if subset_dir == "train":
+                img_dir = os.path.join(dataset_dir, image_id)
+            else:
+                img_dir = dataset_dir
+
             self.add_image(
                 "glomerulus",
                 image_id=image_id,
-                path=os.path.join(dataset_dir, image_id, "images/{}.jpg".format(image_id)))
+                path=os.path.join(img_dir, "images/{}.jpg".format(image_id)))
 
     def load_mask(self, image_id):
         """Generate instance masks for an image.
@@ -225,6 +249,90 @@ class GlomerulusDataset(utils.Dataset):
         else:
             super(self.__class__, self).image_reference(image_id)
 
+############################################################
+#  Mask to .roi utils
+############################################################
+
+def contour_from_mask(mask):
+    # cv2.findContours only works on binary image
+    mask = mask.astype('uint8')
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    return contours[0] #there should be only one contour
+
+def roi_from_contour(name, cnt):
+    # using roi format in read-roi package
+    x = cnt[:,0,0]
+    y = cnt[:,0,1]
+    dict_data = {
+        'type':'freehand',
+        'x': list(x),
+        'y': list(y),
+        'n':len(x),
+        'width':0,
+        'name':name,
+        'position':0
+    }
+    roi = { name : dict_data }
+    return(roi)
+
+def bytes_from_roi(roi):
+# inspired from https://gist.github.com/dabasler/c4b885164db5c845cd20688f4a00cab0
+
+    for name in roi:
+        n = roi[name]['n'] # number of points
+        x = roi[name]['x'] # x coordinates
+        y = roi[name]['y'] # y coordinates
+
+    x = [int(xi) for xi in x] # make sure that x is a list of python int type (not numpy.int type)
+    y = [int(yi) for yi in y] # make sure that x is a list of python int type (not numpy.int type)
+
+    data=bytearray()       # buffer
+
+    # HEADER 1
+    data += "Iout".encode()         # file always starts with 'Iout'
+    data += (227).to_bytes(2,'big') # (int16) version number = 227
+    data += (7).to_bytes(1,'big')   # (int8) type of roi - 7 means 'freehand'
+    data += (0).to_bytes(1,'big')   # (int8) code for subtype - 0 here
+    top = min(y)                    # need to convert to python int because numpy int doesn't work with to_bytes
+    data += (top).to_bytes(2,'big') # (int16) lower y coordinates
+    left = min(x)
+    data += (left).to_bytes(2,'big')# (int16) lower x coordinates
+    bottom = max(y)
+    data += (bottom).to_bytes(2,'big')# (int16) upper y coordinates
+    right = max(x)
+    data += (right).to_bytes(2,'big') # (int16) upper x coordinates
+    data += n.to_bytes(2,'big')     # (int16) total number of coordinates
+    for i in range(42):             # unused bytes
+        data += b'\x00'
+    h2offset_index = len(data)
+    data += (0).to_bytes(4,'big')   # (int32) header2 offset - set to 0 for now, will change at the end
+
+    # COORDINATES
+    for x_i in x: # (int16) array of x coordinates, with offset
+        data += (x_i - left).to_bytes(2,'big')
+    for y_i in y: # (int16) array of y coordinates, with offset
+        data += (y_i - top).to_bytes(2,'big')
+
+    # HEADER2
+    h2offset = len(data)
+    for i in range(16):             # header2 start and C,Z,T positions - unused bytes
+        data += b'\x00'
+    nameoffset_index = len(data)
+    data += (0).to_bytes(4,'big')   # (int32) name offset - set to 0 for now, will change at the end
+    data += (len(name)).to_bytes(4,'big')  # (int32) name length
+    for i in range(40):             # unused bytes
+        data += b'\x00'
+
+    # FILE NAME
+    nameoffset = len(data)
+    for c in name:                  # name of the roi file, encoded every other byte
+        data += b'\x00' + c.encode()
+
+    # write offsets retrospectively
+    data[h2offset_index:h2offset_index+4]=(h2offset).to_bytes(4,'big')
+    data[nameoffset_index:nameoffset_index+4]=(nameoffset).to_bytes(4,'big')
+
+    return data
 
 ############################################################
 #  Training
@@ -268,7 +376,7 @@ def train(model, dataset_dir, subset):
     print("Train all layers")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=40,
+                epochs=45,
                 augmentation=augmentation,
                 layers='all')
 
@@ -277,13 +385,17 @@ def train(model, dataset_dir, subset):
 ############################################################
 
 def detect(model, dataset_dir, subset):
-    """Run detection on images in the given directory."""
+    """Run detection on images in the given directory.
+    and saves image, masks and rois in the result folder
+    """
+
+    assert subset not in ['train','val'], "trying to run detection on train or val set => abort"
     print("Running on {}".format(dataset_dir))
 
-    # Create directory
+    # Create results directory
     if not os.path.exists(RESULTS_DIR):
         os.makedirs(RESULTS_DIR)
-    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
+    submit_dir = "{subset}_detect_{:%Y%m%dT%H%M%S}".format(subset,datetime.datetime.now())
     submit_dir = os.path.join(RESULTS_DIR, submit_dir)
     os.makedirs(submit_dir)
 
@@ -292,31 +404,57 @@ def detect(model, dataset_dir, subset):
     dataset.load_glomerulus(dataset_dir, subset)
     dataset.prepare()
     # Load over images
-    submission = []
+
     for image_id in dataset.image_ids:
         # Load image and run detection
         image = dataset.load_image(image_id)
+        img_name = dataset.image_info[image_id]["id"]
         # Detect objects
         r = model.detect([image], verbose=0)[0]
-        # Encode image to RLE. Returns a string of multiple lines
-        source_id = dataset.image_info[image_id]["id"]
+
         # Save image with masks
         visualize.display_instances(
             image, r['rois'], r['masks'], r['class_ids'],
             dataset.class_names, r['scores'],
             show_bbox=False, show_mask=False,
             title="Predictions")
-        plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+        plt.savefig("{}/{}_result.png".format(submit_dir, img_name ))
 
-    # Save to csv file
-    submission = "ImageId,EncodedPixels\n" + "\n".join(submission)
-    file_path = os.path.join(submit_dir, "submit.csv")
-    with open(file_path, "w") as f:
-        f.write(submission)
-    print("Saved to ", submit_dir)
+        # creation result folder architecture
 
+        subset_dir = os.path.join(dataset_dir,subset)
+        img_dir = os.path.join(submit_dir,"images")
+        os.makedirs(img_dir)
+        # copy image in result folder
+        shutils.copyfile(dataset.image_info[image_id]["path"], os.path.join(img_dir,img_name+".jpg"))
 
-############################################################
+        mask_dir = os.path.join(img_dir,"masks")
+        os.makedirs(mask_dir)
+
+        roi_dir = os.path.join(img_dir,"rois")
+        os.makedirs(roi_dir)
+
+        # save masks and rois
+        for i in range(r['masks'].shape[2]): # shape = (h)x(w)x(number of masks)
+            mask = (r['masks'][:,:,i]*255).astype('uint8')
+            mask_name = "{}-{}".format(img_name,i)
+            skimage.io.imsave("{}/{}.png".format(mask_dir, mask_name),mask)
+
+            # extract roi from mask
+            cnt = contour_from_mask(mask)
+            roi = roi_from_contour(mask_id+'.roi',cnt)
+            bytes = bytes_from_roi(roi)
+            with open(os.path.join(roi_dir,mask_name+'.roi'), "wb") as file:
+                file.write(bytes)
+
+        # Create a ZipFile Object
+        with ZipFile(img_name + '_roi.zip', 'w') as zipObj:
+           # Add multiple files to the zip
+           for file in next(os.walk(roi_dir))[2]:
+               if file.endswith('.roi'):
+                   zipObj.write(file)
+
+###########################################################
 #  Command Line
 ############################################################
 
